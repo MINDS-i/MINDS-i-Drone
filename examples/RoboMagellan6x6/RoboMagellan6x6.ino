fc77aa0428ec360a2cf19f3d4e509f88cc4980ef
Brett Menzies <scimor5@gmail.com>
2014-08-19 14:57:54 -0700

    Moved more settings into data exchange system

--- examples/RoboMagellan6x6/RoboMagellan6x6.ino
+++ examples/RoboMagellan6x6/RoboMagellan6x6.ino
@@ -9,58 +9,38 @@
 #include "HLAverage.h"
 #include "wiring_private.h"
 
-//resync is sketchy
-
-//40/50 works great
-
+//Constants that should never change during driving and never/rarely tuned
 const uint8_t VoltagePin  = 67;
 const uint8_t LEDpin[]    = {25, 26, 27}; //blue, yellow, red
 const uint8_t PingPin[]	  = {A4, A1, A0, A2, A3}; //left to right
 const uint8_t ServoPin[]  = {12, 11,  8};//drive, steer, backS; APM 1,2,3 resp.
 const uint8_t RadioPin[]  = {2, 3}; //drive, steer
-const int SCHEDULE_DELAY  = 20;
-const int REVTHROW		  = 20;
-const int REVSPEED        = 75;
-const int STOP_TIME       = 1500;//time to coast to stop
-const int DANGER_TIME     = STOP_TIME+800; //time to backup after last sighting
+const double  pAngle[5]   = { 79.27l, 36.83l, 0.l, -36.83l, -79.27l};
+const double  dPlsb       = 4.f/float(0xffff); //dps Per leastSigBit for gyro
+const int ScheduleDelay   = 22;
 const uint16_t warn[]     = {1000, 1600, 2500, 1600, 1000};
-const double POINT_RADIUS = .001; //in miles, margin for error in rover location
-const double PING_CALIB   = 1400.l;
-const double pAngle[5]    = { 79.27l, 36.83l, 0.l, -36.83l, -79.27l};
-const double dPlsb        = 4.f/float(0xffff); //dps Per leastSigBit for gyro
+const double PointRadius  = .001; //in miles, margin for error in rover location
 
-/*double lineGravity  = .75; //line return factor
-int STEERTHROW	  	= 45; //degrees from far turn to center
-int MIN_FWD		 	= 107;
-int MAX_FWD		  	= 115;*/
-
-#define lineGravity  manager.getFloat(10)
-#define STEERTHROW   manager.getInt  (11)
-#define STEER_STYLE  manager.getInt  (12)
-#define STEER_FACTOR manager.getFloat(13)
-#define MIN_FWD      manager.getInt  (14)
-#define MAX_FWD      manager.getInt  (15)
-#define BLUE_LED     manager.getBool (16)
-
+//Global variables used throught the program
 HardwareSerial *CommSerial = &Serial;
-NMEA			nmea(Serial1);
 CommManager		manager(CommSerial);
+NMEA			nmea(Serial1);
 Point			location(0,0);
 Point			backWaypoint(0,0);
 HLA				lowFilter (600000, 0);//10 minutes
 HLA				highFilter(    10, 0);//10 milliseconds
-HLA 			pitch(  75, 0);
-HLA 			roll (  75, 0);
+HLA 			pitch( 100, 0);
+HLA 			roll ( 100, 0);
 Servo			servo[3]; //drive, steer, backSteer
-//scheduler, navigation, obstacle, stop
+		//scheduler, navigation, obstacle, stop times
 uint32_t uTime = 0, nTime = 0, oTime = 0, sTime = 0;
 uint32_t gpsHalfTime = 0, gpsTime = 0;
  int32_t Ax,Ay,Az; //used in accelerometer calculations
 uint16_t ping[5] = {20000,20000,20000,20000,20000};
 uint8_t  sIter,pIter; //iterators for scheduler and ping
-double   pathHeading; //all headings are Clockwise=+, -179 to 180, 0=north
+double   pathHeading; //All Headings are Clockwise+, -179 to 180, 0=north
 double   trueHeading;
-double   gyroHalf; //Gyro Heading halfway between gps points
+double   gyroHalf; //Store Gyro Heading halfway between gps points
 double   distance;
 boolean  stop = true;
 boolean  backDir;
@@ -72,11 +52,40 @@ voidFuncPtr schedule[] = {
 							reportLocation,
 							};
 
+//These parameters are synced with the dashboard in real time
+//The defaults are set during setup and sent to the dash when a connection is
+//made; after that they can be changed in the dash and changes will
+//be sent to the robot.
+#define LINEGRAVITY  manager.getFloat(10)
+#define STEERTHROW   manager.getInt  (11)
+#define STEER_STYLE  manager.getInt  (12)
+#define STEER_FACTOR manager.getFloat(13)
+#define MIN_FWD      manager.getInt  (14)
+#define MAX_FWD      manager.getInt  (15)
+#define REVTHROW     manager.getInt  (16)
+#define REVSPEED     manager.getInt  (17)
+#define PING_WEIGHT  manager.getFloat(18)
+#define COAST_TIME   manager.getInt  (19)
+#define DANGER_TIME  manager.getInt  (20)+manager.getInt(19)
+void setTuningDefaults(){
+	manager.setData(10, .50);  //LINEGRAVITY
+	manager.setData(11, 45);   //STEERTHROW
+	manager.setData(12, 1);    //STEER_STYLE
+	manager.setData(13, 1.0);  //STEER_FACTOR
+	manager.setData(14, 107);  //MIN_FWD
+	manager.setData(15, 115);  //MAX_FWD
+	manager.setData(16, 20);   //REVERSE_STEER_THROW
+	manager.setData(17, 75);   //REVERSE_SPEED
+	manager.setData(18, 1400.);//PING_WEIGHT
+	manager.setData(19, 1500); //COAST_TIME
+	manager.setData(20, 800);  //Minumum Backup Time
+}
+
 void setup() {
 	Serial1.begin(38400);
 	CommSerial->begin(Protocol::BAUD_RATE);
 	InitMPU();
-	pinMode(40, OUTPUT); digitalWrite(40, HIGH);
+	pinMode(40, OUTPUT); digitalWrite(40, HIGH); //SPI select pin
 	for(int i=0; i<3; i++) pinMode(LEDpin[i], OUTPUT);
 	for(int i=0; i<3; i++) servo[i].attach(ServoPin[i]);
 	output(90,90);
@@ -86,19 +95,14 @@ void setup() {
 	sendGPSMessage(0x06, 0x00, 0x0014, CFG_PRT);
 	sendGPSMessage(0x06, 0x24, 0x0024, Pedestrian_Mode);
 
-	delay(1000);
+	delay(2000);
 	calibrateGyro(); //this also takes one second
 
 	getRadio(RadioPin[0]);//start radio interrupts
 	manager.requestResync();
 	uTime = millis();
 
-	manager.setData(10, .50); //lineGravity
-	manager.setData(11, 45);  //STEERTHROW
-	manager.setData(12, 0);   //STEER_STYLE
-	manager.setData(13, 1.82);//STEER_FACTOR
-	manager.setData(14, 107); //MIN_FWD
-	manager.setData(15, 115); //MAX_FWD
+	setTuningDefaults();
 }
 
 void loop(){
@@ -106,7 +110,7 @@ void loop(){
 	updateGPS();
 	updateGyro();
 	if(uTime <= millis()){
-		uTime += SCHEDULE_DELAY;
+		uTime += ScheduleDelay;
 		schedule[sIter]();
 		sIter++;
 		sIter = sIter%(sizeof(schedule)/sizeof(*schedule));
@@ -123,7 +127,7 @@ void navigate(){
 			output(90, 90);
 			sTime = millis();
 			backDir = ping[0]<ping[4];
-		} else if(sTime+STOP_TIME < millis()){
+		} else if(sTime+COAST_TIME < millis()){
 			if(backDir) output(REVSPEED, 90-REVTHROW);
 			else 		output(REVSPEED, 90+REVTHROW);
 		}
@@ -142,7 +146,8 @@ void navigate(){
 				outputAngle = atan( angularError*PI/180.l )*(2*STEERTHROW/PI);
 				break;
 			case 1:
-				outputAngle = ((angularError/180.l)*(angularError/180.l))*(2.l*STEERTHROW);
+				outputAngle = ((angularError/180.l)*(angularError/180.l));
+				outputAngle *=(2.l*STEERTHROW);
 				if(angularError < 0) outputAngle *= -1;
 				break;
 			default:
@@ -154,7 +159,7 @@ void navigate(){
 		x = cos(toRad(outputAngle));
 		y = sin(toRad(outputAngle));
 		for(int i=0; i<5; i++){
-			double tmp = ping[i]/PING_CALIB;
+			double tmp = ping[i]/PING_WEIGHT;
 			tmp *= tmp;
 			x += cos(toRad(pAngle[i]))/tmp;
 			y += sin(toRad(pAngle[i]))/tmp;
@@ -189,8 +194,8 @@ void updateGPS(){
 void waypointUpdated(){
 	if(manager.numWaypoints() > 0 && !nmea.getWarning()){
 		stop = false;
-		distance = calcDistance(manager.getTargetWaypoint(), location);
-		if(distance > POINT_RADIUS) return;
+ 		distance = calcDistance(manager.getTargetWaypoint(), location);
+		if(distance > PointRadius)  return;
 
 		if(manager.getTargetIndex() < manager.numWaypoints()-1){
 			backWaypoint = manager.getTargetWaypoint();
@@ -260,7 +265,7 @@ void positionChanged(){
 		double AB    = calcHeading(backWaypoint, manager.getTargetWaypoint());
 		double AL    = calcHeading(backWaypoint, location);
 		double d     = cos(toRad(AL-AB))*calcDistance(backWaypoint, location);
-		double D     = d + (full-d)*(1.l-lineGravity);
+		double D     = d + (full-d)*(1.l-LINEGRAVITY);
 		Point target = extrapPosition(backWaypoint, AB, D);
 		pathHeading  = calcHeading(location, target);
 	}
@@ -283,10 +288,9 @@ void reportLocation(){
 	manager.setData(Protocol::DATA_ROLL,       roll.get()*180/PI);
 	manager.setData(Protocol::DATA_SPEED,      nmea.getGroundSpeed());
 	manager.setData(Protocol::DATA_VOLTAGE,    voltage);
-	digitalWrite(LEDpin[0], BLUE_LED);
-	manager.setData(31, manager.numWaypoints());
 	manager.setData(20, manager.getTargetWaypoint().degLatitude () );
 	manager.setData(21, manager.getTargetWaypoint().degLongitude() );
+	manager.setData(22, manager.numWaypoints());
 }
 
 void calibrateGyro(){ //takes one second
