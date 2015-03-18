c576d9c85cc9781bbc671b001822837422811464
Brett Menzies <scimor5@gmail.com>
2015-03-18 13:41:32 -0700

    accl and gyro tests

--- examples/APM_AcclTest/APM_AcclTest.ino
+++ examples/APM_AcclTest/APM_AcclTest.ino
@@ -2,12 +2,42 @@
 #include "SPI.h"
 #include "Servo.h"
 #include "DroneLibs.h"
-/*
-Accelerometer, Balancing a plate attached to a servo single axis.
-*/
-void setup(){
-	
+
+const float INT_PERIOD = 5000;
+
+Settings        settings(eeStorage::getInstance());
+MpuSensor       mpu;
+InertialSensor* sens[1] = {&mpu};
+InertialManager sensors(sens, 1);
+AcclOnly        orientation;
+
+const float I = 4.0f;
+float integral;
+
+Servo output;
+
+void isrCallback(){
+    sensors.update(orientation);
 }
+
+void setup() {
+    Serial.begin (9600);
+    mpu.tuneAccl(settings.getAccelTune());
+    sensors.start();
+    sensors.calibrate();
+    output.attach(A0);
+    startInterrupt(isrCallback, INT_PERIOD);
+}
+
 void loop(){
-	
+    Quaternion attitude = orientation.getAttitude();
+    float pitch = attitude.getPitch();
+    Serial.print(pitch);
+    Serial.print("\t");
+    Serial.print(integral);
+    Serial.print("\n");
+
+    integral += I*pitch;
+    integral = constrain(integral, -90, 90);
+    output.write(integral+90);
 }
--- examples/APM_GyroTest/APM_GyroTest.ino
+++ examples/APM_GyroTest/APM_GyroTest.ino
@@ -2,12 +2,61 @@
 #include "SPI.h"
 #include "Servo.h"
 #include "DroneLibs.h"
-/*
-Gyro, Balancing a plate attached to a servo single axis.
-*/
-void setup(){
-	
+const float INT_PERIOD = 5000;
+
+Settings        settings(eeStorage::getInstance());
+MpuSensor       mpu;
+InertialSensor* sens[1] = {&mpu};
+InertialManager sensors(sens, 1);
+GyroOnly        orientation;
+
+const float I = 4.0f;
+float integral;
+
+Servo output;
+
+void isrCallback(){
+    sensors.update(orientation);
 }
+
+void setup() {
+    Serial.begin (9600);
+    mpu.tuneAccl(settings.getAccelTune());
+    sensors.start();
+    sensors.calibrate();
+    output.attach(A0);
+    startInterrupt(isrCallback, INT_PERIOD);
+}
+
 void loop(){
-	
+    Quaternion attitude = orientation.getAttitude();
+
+    uint8_t oldSREG = SREG;
+    cli();
+    uint32_t p = -micros();
+    float pitch = attitude.getPitch();
+    float roll  = attitude.getRoll();
+    p += micros();
+
+    uint32_t q = -micros();
+    Vec3 ref(0,0,-1);
+    ref.rotateBy(attitude);
+    float vPitch = asin(ref[0]);
+    float vRoll  = asin(-ref[1]);
+    q += micros();
+    SREG = oldSREG;
+
+    Serial.print(p);
+    Serial.print("\t");
+    Serial.print(q);
+    Serial.print("\t");
+    Serial.print(pitch);
+    Serial.print(roll);
+    Serial.print(vPitch);
+    Serial.print("\n");
+
+
+    integral += I*pitch;
+    integral = constrain(integral, -90, 90);
+    output.write(integral+90);
 }
