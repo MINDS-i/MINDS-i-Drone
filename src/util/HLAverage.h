1a625b619cffa99b6f7f34be7469877697d58a4b
Brett Menzies <scimor5@gmail.com>
2015-07-24 12:09:15 -0700

    Converted to floats

--- src/util/HLAverage.h
+++ src/util/HLAverage.h
@@ -9,36 +9,39 @@
 
 class HLA{
 private:
-	double halfLife;
-	double value;
+	float halfLife;
+	float value;
 	uint32_t time;
 public:
-	HLA(double half, double init): halfLife(half*1000.l), value(init) {
+	HLA(float half, float init): halfLife(half*1000.l), value(init) {
 		time=micros();
 	}
-	double update(double newValue){
+	float update(float newValue){
 		if(isnan(newValue)) return value;
-		double dt = double(micros()-time);
-		double factor = pow(2.l, -dt/halfLife);
+		float dt = float(micros()-time);
+		float factor = pow(2.l, -dt/halfLife);
 		value = (1.l-factor)*newValue + factor*value;
 		time = micros();
 		return value;
 	}
-	double get(){
+	float get(){
 		return value;
 	}
-	double millisSinceUpdate(){
+	float millisSinceUpdate(){
 		return (micros()-time)/1000.l;
 	}
 	uint32_t microsSinceUpdate(){
 		return micros()-time;
 	}
-	void set(double newValue){
+	void set(float newValue){
 		if(isnan(newValue)) return;
 		value = newValue;
 	}
 	void resetTime(){
 		time = micros();
+	}
+	void setHalfLife(float half){
+		halfLife = half*1000.0f;
 	}
 };
 
