44d288a092267eeeebff48fde132378bd583a19a
Brett Menzies <scimor5@gmail.com>
2015-07-31 11:23:33 -0700

    Added pid parameter setter methods

--- src/util/PIDparameters.h
+++ src/util/PIDparameters.h
@@ -10,22 +10,44 @@ struct PIDparameters{
         : P(p), I(i), D(d), acc(0), lowerBound(0), upperBound(1) {};
     PIDparameters(float p, float i, float d, float l, float u)
         : P(p), I(i), D(d), acc(0), lowerBound(l), upperBound(u) {};
-    void setIdeal(float kp, float ki, float kd){
-        P = kp;
-        I = ki;
-        D = kd;
-    }
-    void setStandard(float kp, float ti, float td){
-        P = kp;
-        I = kp/ti;
-        D = kp*td;
+    void resetAccumulator(){
+        acc = 0;
     }
     void setBounds(float lower, float upper){
         lowerBound = lower;
         upperBound = upper;
     }
-	void resetAccumulator(){
-        acc = 0;
+
+    void setStandard(float kp, float ti, float td){
+        P = kp;
+        I = kp/ti;
+        D = kp*td;
+    }
+    void setStandardP(float kp){
+        I *= (kp/P); //recalculate I and D with new parameter
+        D *= (kp/P);
+        P = kp;
+    }
+    void setStandardI(float ti){
+        I = P/ti;
+    }
+    void setStandardD(float td){
+        D = P*td;
+    }
+
+    void setIdeal(float kp, float ki, float kd){
+        P = kp;
+        I = ki;
+        D = kd;
+    }
+    void setIdealP(float kp){
+        P = kp;
+    }
+    void setIdealI(float ki){
+        I = ki;
+    }
+    void setIdealD(float kd){
+        D = kd;
     }
 };
 #endif
