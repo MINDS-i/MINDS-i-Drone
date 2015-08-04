802c0066686c49f26078388f758dfd0d6cbfa16d
Brett Menzies <scimor5@gmail.com>
2015-08-04 14:24:42 -0700

    Horizon uses full cascading PID

--- src/output/Horizon.h
+++ src/output/Horizon.h
@@ -6,25 +6,25 @@
 
 class Horizon : public FlightStrategy {
 private:
-    PIDcontroller  pitchPID, rollPID;
-    PIDcontroller  yawPID;
+    PIDcontroller  pitchPID, pError;
+    PIDcontroller   rollPID, rError;
+    PIDcontroller    yawPID, yError;
+    ThrottleCurve* tCurve;
     float          throttle;
     float          pitch, roll, yaw;
     float          velFac, yawFac;
     float          tiltCompLimit;
     bool           standbyOn;
-    ThrottleCurve* tCurve;
 public:
     float testPoint[4];
-    Horizon(PIDparameters* p, PIDparameters* r, PIDparameters* y)
-        : pitchPID(p), rollPID(r), yawPID(y),
-          velFac(1), yawFac(1), tiltCompLimit(1), standbyOn(false) {}
-    Horizon(PIDparameters* p, PIDparameters* r, PIDparameters* y, float velocityFactor)
-        : pitchPID(p), rollPID(r), yawPID(y),
-          velFac(velocityFactor), yawFac(1), tiltCompLimit(1), standbyOn(false) {}
-    Horizon(PIDparameters* p, PIDparameters* r, PIDparameters* y, ThrottleCurve* tc)
-        : pitchPID(p), rollPID(r), yawPID(y),
-          velFac(1), yawFac(1), tiltCompLimit(1), standbyOn(false), tCurve(tc) {}
+    Horizon(PIDparameters* pitchI, PIDparameters* pitchO,
+            PIDparameters*  rollI, PIDparameters*  rollO,
+            PIDparameters*   yawI, PIDparameters*   yawO,
+            ThrottleCurve* tc ) :
+                pitchPID(pitchI), pError(pitchO),
+                 rollPID( rollI), rError( rollO),
+                  yawPID(  yawI), yError(  yawO),
+                tCurve(tc) {}
     void update(OrientationEngine& orientation, float (&torques)[4]){
         if(standbyOn){
             torques[0] = 0;
@@ -33,27 +33,25 @@ public:
             torques[3] = 0;
             return;
         }
-        float pError = velFac * (pitch - orientation.getPitch());
-        float rError = velFac * (roll  - orientation.getRoll());
-        float yError = yawFac * distanceRadian(orientation.getYaw(), yaw);
-        pitchPID.set(pError);
-        rollPID.set(rError);
-        yawPID.set(yError);
+
+        //calculate outer loop
+        const float p = pError.update(orientation.getPitch() - pitch          );
+        const float r = rError.update(orientation.getRoll()  - roll           );
+        const float y = yError.update(distanceRadian(yaw,orientation.getYaw()));
+        //set inner loops with outer calculations
+        pitchPID.set(p);
+        rollPID.set(r);
+        yawPID.set(y);
+        //set torques from inner PID loop calculations
         torques[0] = pitchPID.update(orientation.getPitchRate()*1024.f);//1024 from rad/millisecond
         torques[1] = rollPID.update(orientation.getRollRate()*1024.f);  //to rad/second
         torques[2] = yawPID.update(orientation.getYawRate()*1024.f);
         torques[3] = throttle;
-        /*
-        Quaternion attitude = orientation.getAttitude();
-        Vec3 down(0,0,1);
-        down.rotateBy(attitude);
-        float corr = max(down[2], tiltCompLimit); //capped at 115%
-        torques[3] = throttle/corr;
-        */
-        testPoint[0] = yaw;
-        testPoint[1] = orientation.getYaw();
-        testPoint[2] = yError;
-        testPoint[3] = torques[2];
+        //debugging test points
+        testPoint[0] = pitch;
+        testPoint[1] = orientation.getPitch();
+        testPoint[2] = p;
+        testPoint[3] = torques[0];
     }
     void standby(){
         standbyOn = true;
@@ -64,8 +62,11 @@ public:
     }
     void reset(){
         pitchPID.clearAccumulator();
+        pError.clearAccumulator();
         rollPID.clearAccumulator();
+        rError.clearAccumulator();
         yawPID.clearAccumulator();
+        yError.clearAccumulator();
     }
     void set(float (&setps)[4]){
         if(standbyOn) activate();
@@ -77,18 +78,6 @@ public:
         this->roll     = roll;
         this->yaw      = yaw;
         this->throttle = tCurve->get(throttle);
-    }
-    float getVelFac(){
-        return velFac;
-    }
-    void setVelFac(float vf){
-        velFac = vf;
-    }
-    float getYawFac(){
-        return yawFac;
-    }
-    void setYawFac(float yf){
-        yawFac = yf;
     }
     float getTiltCompLimit(){
         return tiltCompLimit;
