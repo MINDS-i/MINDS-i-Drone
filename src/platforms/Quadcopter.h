478e2039155f9c135a4146f9fd6dbd1883d6418b
Brett Menzies <scimor5@gmail.com>
2015-08-04 14:24:20 -0700

    More PID parameters full cascading PID

--- src/platforms/Quadcopter.h
+++ src/platforms/Quadcopter.h
@@ -21,10 +21,13 @@ Output_t esc[4] =
 OutputDevice* outDev[4] = {&esc[0], &esc[1], &esc[2], &esc[3]};
 OutputManager output(outDev);
 
-PIDparameters pitchPID(0,0,0,-1,1), rollPID(0,0,0,-1,1);
-PIDparameters yawPID(0,0,0,-1,1);
+PIDparameters attPID(0,0,0,-100,100), yawPID(0,0,0,-1,1);
+PIDparameters attVel(0,0,0,-100,100), yawVel(0,0,0,-1,1);
 ThrottleCurve throttleCurve(0.33, 0.4);
-Horizon       horizon(&pitchPID, &rollPID, &yawPID, &throttleCurve);
+Horizon       horizon(&attPID, &attVel,
+                      &attPID, &attVel,
+                      &yawPID, &yawVel,
+                      &throttleCurve);
 HLA           altitude(1, 0);
 
 ///////////
@@ -41,14 +44,6 @@ void isrCallback() {
     output.update(orientation);
     toc(0);
 }
-void updatePID(float v){
-    using namespace AirSettings;
-    float p = settings.get(ATT_P_TERM);
-    float i = settings.get(ATT_I_TERM);
-    float d = settings.get(ATT_D_TERM);
-    pitchPID.setIdeal(p,i,d);
-    rollPID.setIdeal(p,i,d);
-}
 void changeInterruptPeriod(float newPeriod){
     if(newPeriod < MINIMUM_INT_PERIOD) newPeriod = MINIMUM_INT_PERIOD;
     startInterrupt(isrCallback, newPeriod);
@@ -56,26 +51,28 @@ void changeInterruptPeriod(float newPeriod){
 void setupSettings(){
      /*
      TTC 0.85
-     PID: 0.6, 0.06, 0.0225, 6.5
-          0.8, 4.00, 0.023 , 6.5
-            yaw: -1.0, -0.4, 0.0, 8.0
+     PID: 0.6, 0.06 0.0225; 6.5 0.0 0.0
+          0.8, 4.00 0.023 ; 6.5 0.0 0.0
+          0.2  0.0  0.005;  8.0 0.5 0.5
      */
     using namespace AirSettings;
     settings.attach(INT_PERIOD, 6500  , &changeInterruptPeriod );
     settings.attach(INRT_U_FAC, 0.075f, callback<RCFilter, &orientation, &RCFilter::setwGain>);
     settings.attach(GYRO_CMP_F, 0.99999f, callback<RCFilter, &orientation, &RCFilter::setRateGain>);
     settings.attach(TILT_CMP_L, 1.00f , callback<Horizon, &horizon, &Horizon::setTiltCompLimit>);
-    settings.attach(ATT_P_TERM, 0.550f, &updatePID);
-    settings.attach(ATT_I_TERM, 0.060f, &updatePID);
-    settings.attach(ATT_D_TERM, 0.023f, &updatePID);
-    settings.attach(ATT_V_TERM, 6.50f , callback<Horizon, &horizon, &Horizon::setVelFac>);
-    settings.attach(YAW_P_TERM, 0.00f , callback<PIDparameters, &yawPID, &PIDparameters::setIdealP>);
+    settings.attach(ATT_P_TERM, 0.550f, callback<PIDparameters, &attPID, &PIDparameters::setIdealP>);
+    settings.attach(ATT_I_TERM, 0.060f, callback<PIDparameters, &attPID, &PIDparameters::setIdealI>);
+    settings.attach(ATT_D_TERM, 0.023f, callback<PIDparameters, &attPID, &PIDparameters::setIdealD>);
+    settings.attach(ATT_VP_TERM,6.50f , callback<PIDparameters, &attVel, &PIDparameters::setIdealP>);
+    settings.attach(ATT_VI_TERM,0.00f , callback<PIDparameters, &attVel, &PIDparameters::setIdealI>);
+    settings.attach(ATT_VD_TERM,0.00f , callback<PIDparameters, &attVel, &PIDparameters::setIdealD>);
+    settings.attach(YAW_P_TERM, -1.0f , callback<PIDparameters, &yawPID, &PIDparameters::setIdealP>);
     settings.attach(YAW_I_TERM, 0.00f , callback<PIDparameters, &yawPID, &PIDparameters::setIdealI>);
     settings.attach(YAW_D_TERM, 0.00f , callback<PIDparameters, &yawPID, &PIDparameters::setIdealD>);
-    settings.attach(YAW_V_TERM, 1.00f , callback<Horizon, &horizon, &Horizon::setYawFac>);
+    settings.attach(YAW_V_TERM, 8.00f , callback<PIDparameters, &yawVel, &PIDparameters::setIdealP>);
     settings.attach(HOVER_THL , 0.40f , callback<ThrottleCurve, &throttleCurve, &ThrottleCurve::setHoverPoint>);
     settings.attach(THL_LINITY, 0.30f , callback<ThrottleCurve, &throttleCurve, &ThrottleCurve::setLinearity>);
-    settings.attach(BARO_HL   , 1.00f , callback<HLA, &altitude, &HLA::setHalfLife>);
+    settings.attach(BARO_HL   , 500.f , callback<HLA, &altitude, &HLA::setHalfLife>);
 }
 void arm(){
     delay(500);
