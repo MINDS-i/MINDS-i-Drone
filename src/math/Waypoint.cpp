9c9dab13018d7b2d2d432f0c83fb80670e064a67
Brett Menzies <scimor5@gmail.com>
2016-01-27 09:21:25 -0800

    Updated const to reflect namespace change

--- src/math/Waypoint.cpp
+++ src/math/Waypoint.cpp
@@ -1,11 +1,11 @@
 #include "Waypoint.h"
 
 #ifndef STAND_ALONE_TEST
-#include "comms/Protocol.h"
+#include "comms/DroneProtocol.h"
 float Waypoint::getAltitude(){
-    return ((double)extra)/((double)Protocol::U16_FIXED_FACTOR);
+    return ((double)extra)/((double)DroneProtocol::U16_FIXED_FACTOR);
 }
 float Waypoint::getApproachSpeed(){
-    return ((double)extra)/((double)Protocol::U16_FIXED_FACTOR);
+    return ((double)extra)/((double)DroneProtocol::U16_FIXED_FACTOR);
 }
 #endif
