56b3482c5e88270441082616b52cf8a2132f2279
Brett Menzies <scimor5@gmail.com>
2016-03-15 16:46:00 -0700

    Document CommManager public methods

--- src/comms/CommManager.h
+++ src/comms/CommManager.h
@@ -19,6 +19,59 @@ using namespace DroneProtocol;
 const uint8_t BUFF_LEN = 32;
 
 class CommManager{
+public:
+	CommManager(Stream *inStream, Storage<float> *settings);
+	/** parse new data that has arrived */
+	void  	 update();
+	/**	Set the callcack to fire when an initial connection SYNC is received */
+	void	 setConnectCallback(void (*call)(void));
+	/** Set the callback to fire when ESTOP command is received */
+	void	 setEStopCallback(void (*call)(void));
+	/**
+	 * Return weather the robot should stop or loop around when the last
+	 * waypoint is reached
+	 */
+	bool 	 loopWaypoints();
+	/** return the value of setting number `id` */
+	float    getSetting(uint8_t id);
+	/** retrieve the index of the rover's current target */
+	uint16_t getTargetIndex();
+	/** retrieve the number of waypoints in the rover's path */
+	uint16_t numWaypoints();
+	/** retrieve the Waypoint located at the rover's current target */
+	Waypoint getTargetWaypoint();
+	/** retrieve the Waypoint `index` (0 based) in the rover's current path */
+	Waypoint getWaypoint(uint16_t index);
+	/** Transmit telemetry type `id` equals `value` to the Dashboard */
+	void	 sendTelem(uint8_t id , float value);
+	/** Clear the local Waypoint List */
+	void 	 clearWaypointList();
+	/** Request that the Dashboard resynchronize settings and waypoints */
+	void  	 requestResync();
+	/**
+	 *  Set the target to the next waypoint in the list and notify
+	 *  the Dashboard
+	 */
+	void     advanceTargetIndex();
+	/**
+	 *  Set the target to the previous waypoint in the list and notify
+	 *  the Dashboard
+	 */
+	void     retardTargetIndex();
+	/**
+	 * Set the target to the `index` (0 based) waypoint in the list
+	 * notifies the dashboard
+	 */
+	void     setTargetIndex(uint16_t index);
+	/** Set setting number `id` to the value `input`, notifing the dashboard */
+	void     setSetting(uint8_t id,   float input);
+	/** send a message with `msg` payload and `type` in the type field */
+	void     sendString(int type, const char* msg, uint8_t len);
+	/** Send a string to be displayed in the dashboard */
+	void 	 sendString(char const * msg);
+	/** Send an error message to be displayed in the dashboard */
+	void 	 sendError(char const * msg);
+private:
 	Stream 			    *stream;
 	uint8_t 			buf[BUFF_LEN];
 	uint8_t 			bufPos;
@@ -30,28 +83,6 @@ class CommManager{
 	bool				waypointsLooped;
 	void (*connectCallback)(void);
 	void (*eStopCallback)(void);
-public:
-	CommManager(Stream *inStream, Storage<float> *settings);
-	bool 	 loopWaypoints();
-	float    getSetting(uint8_t id);
-	uint16_t getTargetIndex();
-	uint16_t numWaypoints();
-	void	 sendTelem(uint8_t id , float value);
-	void	 setConnectCallback(void (*call)(void));
-	void	 setEStopCallback(void (*call)(void));
-	void 	 clearWaypointList();
-	void  	 requestResync();
-	void  	 update();
-	void     advanceTargetIndex();
-	void     retardTargetIndex();
-	void     setSetting(uint8_t id,   float input);
-	void     setTargetIndex(uint16_t index);
-	void     sendString(int type, const char* msg, uint8_t len);
-	void 	 sendString(char const * msg);
-	void 	 sendError(char const * msg);
-	Waypoint getTargetWaypoint();
-	Waypoint getWaypoint(uint16_t index);
-private:
 	CommManager(const CommManager& copy); //intentionally not implemented
 	boolean recieveWaypoint(waypointSubtype type, uint8_t index, Waypoint point);
 	boolean rightMatch(const uint8_t* lhs, const uint8_t llen,
