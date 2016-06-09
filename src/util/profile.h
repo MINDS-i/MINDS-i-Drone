61d9516aed77bfd27e13623dde310d009cc47988
Brett Menzies <scimor5@gmail.com>
2016-06-09 15:04:11 -0700

    Always inline the profile functions

--- src/util/profile.h
+++ src/util/profile.h
@@ -3,10 +3,10 @@
 #if DEBUG
 const int NUM_PROFILES = 16;
 volatile uint32_t profile[NUM_PROFILES];
-void tic(int i){
+void __attribute__((always_inline)) inline tic(int i){
 	profile[i] = -micros();
 }
-void toc(int i){
+void __attribute__((always_inline)) inline toc(int i){
 	profile[i] += micros();
 }
 #endif
