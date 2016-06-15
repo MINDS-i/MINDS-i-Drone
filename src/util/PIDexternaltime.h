14d5ebdeeffef0c4c017c7ffe62ac2e2c16d28e7
Brett Menzies <scimor5@gmail.com>
2016-06-15 15:42:35 -0700

    Fix stop functionality and document methods

--- src/util/PIDexternaltime.h
+++ src/util/PIDexternaltime.h
@@ -14,15 +14,23 @@ public:
     PIDexternaltime(PIDparameters* pid): param(pid), setPoint(0),
                                          previous(0), acc(0),
                                          stopped(true) {}
+    /** Set the pid parameters to be used */
     void tune(PIDparameters* pid){ param = pid; }
+    /** Clear the accumulator to have no bias */
     void clearAccumulator(){ train(0); }
+    /** Set the controller so the accumulator has a bias of `out` */
     void train(float out){
         acc = constrain(out, param->lowerBound, param->upperBound);
     }
+    /** Set the setpoint used in the pid controller */
     void set(float input){
         setPoint = input;
         stopped = false;
     }
+    /**
+     * Stop the pid controller. This clears the accumulator.
+     * It will output 0 until the next time `set` is called
+     */
     void stop() {
         clearAccumulator();
         stopped = true;
@@ -33,6 +41,8 @@ public:
      * ms - milliseconds since last update
      */
     float update(float current, float ms){
+        if(stopped) return 0;
+
         const float dt = min(ms/1000.0, 1.0); //convert to seconds, cap at 1
 
         const float error  = setPoint-current;
