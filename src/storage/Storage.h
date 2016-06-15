9f3adfcb51f7d5663375e63635140415ebffaf78
Brett Menzies <scimor5@gmail.com>
2016-06-15 16:39:40 -0700

    Better document the Storage interface

--- src/storage/Storage.h
+++ src/storage/Storage.h
@@ -1,17 +1,23 @@
 #ifndef STORAGE_H
 #define STORAGE_H
-/*
-	The storage abstract base class
-	stores an array of values, with callbacks
-	the callbacks will be called when attached and written
-*/
+
 template<typename T>
 class Storage{
 protected:
 	Storage(){}
 public:
+    /**
+     * Attach a callback to location `dataNum` that will be fired every time
+     * it is updated. It will be called immediately after being attached with
+     * the current value
+     */
 	virtual void attachCallback(uint8_t dataNum, void (*call)(T))=0;
+    /**
+     * Update the data at location `dataNum` to `value`, firing attached
+     * callbacks as necessary
+     */
 	virtual void updateRecord(uint8_t dataNum, T value)=0;
+    /** Retreive the data at location `dataNum` */
 	virtual T getRecord(uint8_t dataNum)=0;
 };
 
