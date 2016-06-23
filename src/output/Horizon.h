7c8f8063a0c80c7ff77d189a0330986ac8466736
Brett Menzies <scimor5@gmail.com>
2016-06-23 15:34:00 -0700

    Fix reversed yaw controller

--- src/output/Horizon.h
+++ src/output/Horizon.h
@@ -21,7 +21,7 @@ public:
         //calculate outer loop
         float p = pError.update(orientation.getPitch() - pitch, ms);
         float r = rError.update(orientation.getRoll() - roll, ms);
-        float y = yError.update(distanceRadian(orientation.getYaw(),yaw), ms);
+        float y = yError.update(distanceRadian(yaw,orientation.getYaw()), ms);
         //set inner loops with outer calculations
         pitchPID.set(p);
         rollPID.set(r);
