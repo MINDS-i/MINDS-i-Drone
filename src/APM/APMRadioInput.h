712b083f6105b57ded0885df693c1ad11bc0f825
Brett Menzies <scimor5@gmail.com>
2016-07-18 16:12:06 -0700

    Implement atomic pulse length reads

--- src/APM/APMRadioInput.h
+++ src/APM/APMRadioInput.h
@@ -2,16 +2,17 @@
 #define APMRADIOINPUT_H
 
 #include "Arduino.h"
+#include "util/atomic.h"
 #if defined(__AVR_ATmega2560__)
 
 namespace APMRadio {
-	volatile uint16_t pulse[8]; //APM2.* has 8 input channels
-	volatile static uint16_t pTime;
-
+	static const uint16_t SYNC_PULSE_LENGTH = 42000;
 	static const uint16_t SCALE    = 100;
 	static const uint16_t MINPULSE = 14900;
 	static const uint16_t MAXPULSE = MINPULSE + 180*SCALE;
 	static const uint16_t DEFAULTP = MINPULSE +  90*SCALE;
+
+	volatile uint16_t pulse[8]; //APM2.* has 8 input channels
 
 	/**
 	 * Start tracking the radio inputs on an APM 2.* board
@@ -27,29 +28,33 @@ namespace APMRadio {
 	    for(int i=0; i<8; i++) pulse[i] = DEFAULTP;
 	}
 
-	/** Get the radio signal on channel `num` mapped between 0 and 180 */
-	uint8_t get(uint8_t num){
-		uint16_t val = constrain(pulse[num], MINPULSE, MAXPULSE);
-		return (val-MINPULSE)/SCALE;
+	/** Get the raw radio signal on channel at its highest resolution */
+	uint16_t inline raw(uint8_t num){
+		uint16_t data;
+		ATOMIC_BLOCK(ATOMIC_RESTORESTATE){ data = pulse[num]; }
+		return data;
 	}
 
-	/** Get the raw radio signal on channel at its highest resolution */
-	uint16_t raw(uint8_t num){
-		return pulse[num];
+	/** Get the radio signal on channel `num` mapped between 0 and 180 */
+	uint8_t inline get(uint8_t num){
+		uint16_t val = constrain(raw(num), MINPULSE, MAXPULSE);
+		return (val-MINPULSE)/SCALE;
 	}
 }
 
 ISR(TIMER5_CAPT_vect){
 	static uint8_t cNum; //channel Number
-	uint16_t dt = ICR5 - APMRadio::pTime;
+	static uint16_t previousTriggerTime;
 
-	if (dt > 42000) { //sync pulse detected
+	uint16_t dt = ICR5 - previousTriggerTime;
+	previousTriggerTime = ICR5;
+
+	if (dt > APMRadio::SYNC_PULSE_LENGTH) { //sync pulse detected
 		cNum = 0;
 	} else {
 		APMRadio::pulse[cNum] = dt;
 		cNum = (cNum+1) % 8;
 	}
-	APMRadio::pTime = ICR5;
 }
 
 #endif
