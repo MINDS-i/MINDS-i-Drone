19812c8ecad5e2d02f985cac49764d437fa0f9f5
Brett Menzies <scimor5@gmail.com>
2016-07-26 14:11:10 -0700

    Barometer example includes Altitude filter

--- examples/APM_Barometer/APM_Barometer.ino
+++ examples/APM_Barometer/APM_Barometer.ino
@@ -1,16 +1,12 @@
 #include "Wire.h"
 #include "SPI.h"
 #include "MINDS-i-Drone.h"
-/*
-Altimeter, get a raw value and maybe a converted value to feet?
-(through the Arduino Serial Terminal)
-*/
 
 MS5611 baro;
+Altitude altitude;
 SRAMstorage<float,2> storage;
 Settings settings(&storage);
 CommManager comms(&Serial, &storage);
-Altitude altitude;
 
 void setup(){
     Serial.begin(9600);
@@ -19,16 +15,16 @@ void setup(){
     delay(100);
     baro.calibrate();
 
-    altitude.setup(0.0);
-    settings.attach(0, 0.05, [](float g){ altitude.setBarometerGain(g); });
-    settings.attach(1, 0.02, [](float g){ altitude.setVelocityGain(g); });
+    settings.attach(0, 0.046f, [](float g){ altitude.setBarometerGain(g); });
+    settings.attach(1, 0.020f, [](float g){ altitude.setVelocityGain(g); });
 }
+
 void loop(){
     comms.update();
     baro.update();
     altitude.update(baro.getAltitude());
 
-    static auto timer = Interval::every(10);
+    static auto timer = Interval::every(250);
     if(timer()){
         float temp = baro.getCelsius();
         float bar  = baro.getMilliBar();
@@ -37,7 +33,7 @@ void loop(){
         comms.sendTelem(GROUNDSPEED, altitude.getVelocity());
         comms.sendTelem(VOLTAGE, temp);
         comms.sendTelem(AMPERAGE, bar);
-        comms.sendTelem(ALTITUDE, alt);
-        comms.sendTelem(ALTITUDE+1, altitude.getAltitude());
+        comms.sendTelem(ALTITUDE, altitude.getAltitude());
+        comms.sendTelem(ALTITUDE+1, alt);
     }
 }
