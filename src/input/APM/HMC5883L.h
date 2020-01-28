ae009d1befa4cee31af21c28f79942eb94a2fc77
Bess <60241300+BessPtck@users.noreply.github.com>
2020-01-27 17:29:21 -0800

    fix compiler crash, set compass check at ARM time
    Split into .h and .cpp to fix compiler segementation fault at compile time. Set the status() function which is called when the board is powered on to only set which time of compass is used but not check compass functionality. status() now always returns Sensor::OK. The compass functionality check is now in checkGoodValues which is called by safe() in ardupilot.h. safe() is called when the quadcopter is armed. Compass is now checked for good values when the quadcopter is armed instead of when it is powered.

--- src/input/APM/HMC5883L.h
+++ src/input/APM/HMC5883L.h
@@ -1,5 +1,7 @@
+#ifndef HMC5883L_H
+#define HMC5883L_H
+
 #include "input/InertialManager.h"
-#include "input/Sensor.h"
 #include "util/LTATune.h"
 constexpr auto HMC5883L_MAX_EXPECTED_VALUE_MAG = 1500;
 constexpr auto HMC5883L_MIN_EXPECTED_VALUE_MAG = 50;
@@ -11,8 +13,8 @@ protected:
     static const uint8_t HMC_STATUS_REG = 0x09;
     LTATune LTA;
     uint8_t address;
-    bool isTrueHMC5883L;
 public:
+    bool isTrueHMC5883L;
     HMC5883L(): address(HMC_I2C_ADDR) {}
     HMC5883L(uint8_t addr): address(addr) {}
     void  begin();
@@ -25,104 +27,5 @@ public:
     void  rawValues(int& x, int& y, int& z);
     float getAzimuth();
 };
-void
-HMC5883L::begin(){
-    Wire.begin();
-    Wire.setClock(800000L);
-    delay(10);
+#endif
 
-    Wire.beginTransmission(address);
-    Wire.write((uint8_t) 0x00);
-    Wire.write((uint8_t) 0x70);
-    Wire.endTransmission();
-
-    Wire.beginTransmission(address);
-    Wire.write((uint8_t) 0x01);
-    Wire.write((uint8_t) 0x00);
-    Wire.endTransmission();
-}
-void
-HMC5883L::tune(LTATune t){
-    LTA = t;
-}
-void
-HMC5883L::end(){
-
-}
-bool
-HMC5883L::checkGoodValues() {
-    int m[3];
-    rawValues(m[0], m[1], m[2]);
-    float mag = (float)(m[0] * m[0] + m[1] * m[1]);
-    mag = sqrt(mag);
-    if (mag >= HMC5883L_MAX_EXPECTED_VALUE_MAG || mag <= HMC5883L_MIN_EXPECTED_VALUE_MAG)
-        return false;
-    int zmag = abs(m[2]);
-    return (zmag <= HMC5883L_MAX_EXPECTED_VALUE_MAG);
-}
-Sensor::Status
-HMC5883L::status(){
-    isTrueHMC5883L = true;
-    Wire.beginTransmission(address);
-    Wire.write((uint8_t)0x09);
-    Wire.endTransmission();
-    Wire.requestFrom(address, (uint8_t)0x01);
-    if(Wire.available()>=1){
-        uint8_t status = Wire.read();
-        if((status&0x3) == 1) return Sensor::OK;
-    }
-    isTrueHMC5883L = false;//bad return value either compass failed or is clone. Set compass as clone.
-    if (checkGoodValues()) return Sensor::OK; //check if cloned compass is returning reasonable values, if so go on.
-    /*#HMCFAIL HMC5883L Compass sensor failed contact or reported bad status*/
-    return Sensor::BAD("HMCFAIL");
-}
-void
-HMC5883L::calibrate(){
-
-}
-void
-HMC5883L::update(InertialManager& man, Translator axis){
-    int m[3];
-    rawValues(m[0], m[1], m[2]);
-    float M[3];
-    LTA.calibrate<int>(m,M);
-    man.mag = axis(M);
-}
-void
-HMC5883L::rawValues(int& x, int& y, int& z) {
-    Wire.beginTransmission(address);
-    if (isTrueHMC5883L) {
-        Wire.write((uint8_t)0x02);
-        Wire.write((uint8_t)0x01);
-    }
-    else {
-        Wire.write((uint8_t)0x03);
-    }
-    Wire.endTransmission();
-
-
-    Wire.requestFrom(address, (uint8_t)0x06);
-    if(Wire.available()>=6){
-        uint8_t* d = (uint8_t*) &x;
-        d[1] = Wire.read();
-        d[0] = Wire.read();
-        d = (uint8_t*) &z;
-        d[1] = Wire.read();
-        d[0] = Wire.read();
-        d = (uint8_t*) &y;
-        d[1] = Wire.read();
-        d[0] = Wire.read();
-    }
-}
-float
-HMC5883L::getAzimuth(){
-    int m[3];
-    rawValues(m[0], m[1], m[2]);
-    float M[3];
-    for(int i=0; i<3; i++){
-        M[i]  = m[i];
-        M[i] += LTA.shift[i];
-        M[i] *= LTA.scalar[i];
-    }
-    return atan2(M[0], M[1]);
-}
