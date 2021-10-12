40803942bd0599bb11fec8732c49ab787e75b4e2
Richard Garcia <72397486+richard-hatchbed@users.noreply.github.com>
2021-10-12 11:39:59 -0500

    Reset mission after complete (#5)
    * Reset target to first mission waypoint after mission completion.
    
    * Set default steering bias to zero instead of RDGs system.

--- examples/RoboMagellan6x6/RoboMagellan6x6.ino
+++ examples/RoboMagellan6x6/RoboMagellan6x6.ino
@@ -20,7 +20,7 @@ float k_heading = 0;
 bool heading_lock = false;
 bool new_gps = false;
 
-int8_t steer_bias = -5;
+int8_t steer_bias = 0;
 
 bool driving_straight = false;
 //=============================================//
@@ -1599,6 +1599,7 @@ void waypointUpdated()
 		else
 		{
 			changeDriveState(DRIVE_STATE_STOP);
+			manager.setTargetIndex(0);
 		}
 
 		clearAutoStateFlag(AUTO_STATE_FLAG_APPROACH);
