5b89e8c0a823ae0aa0abeedced13ba36effd140f
BenAndersonInfinetix <68874741+BenAndersonInfinetix@users.noreply.github.com>
2021-11-12 13:33:15 -0800

    Update version.h
    Version bump for RoboMagelland6x6

--- examples/RoboMagellan6x6/version.h
+++ examples/RoboMagellan6x6/version.h
@@ -2,7 +2,7 @@
 #define ROBOMAG6X6_VERSION_H
 
 uint8_t	 version_major = 0;
-uint8_t	 version_minor = 1;
-uint8_t	 version_rev = 11;
+uint8_t	 version_minor = 2;
+uint8_t	 version_rev = 0;
 
 #endif
