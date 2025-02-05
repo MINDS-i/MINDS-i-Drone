8b38d183db67c3af5974d006b97d5710275f09a5
Alex Youngs <alexyoungs@hatchbed.com>
2025-02-05 12:34:46 -0600

    Fix telem reporting order

--- examples/quadcopter/quadcopter.ino
+++ examples/quadcopter/quadcopter.ino
@@ -251,9 +251,9 @@ const telemLine telemetryTable[] = {
     []() { return power.getVoltage(); },            // VOLTAGE
     []() { return power.getAmperage(); },           // AMPERAGE
     []() { return altitude.getAltitude(); },        // ALTITUDE
-    []() { return (float)APMRadio::get(RADIO_THROTTLE); },
-    []() { return (float)APMRadio::get(RADIO_PITCH); },
     []() { return (float)APMRadio::get(RADIO_ROLL); },
+    []() { return (float)APMRadio::get(RADIO_PITCH); },
+    []() { return (float)APMRadio::get(RADIO_THROTTLE); },
     []() { return (float)APMRadio::get(RADIO_YAW); },
     []() { return (float)APMRadio::get(RADIO_SWITCH); },
     []() { return (float)APMRadio::get(RADIO_AUX2); },
